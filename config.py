3cb31c50f2bb42e46ef918b0db01b7cd6c206ea5
rjxff614-web <rjxff614@gmail.com>
2026-05-15 07:07:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-API_ID = 37535960
-API_HASH = "e89c6a21da912026e645f4132bd4eba7"
-BOT_TOKEN = "8721566005:AAHvdIHLgElJRBgdr8WaVaG8UXnndUevZAE"
+import os
+
+API_ID = int(os.getenv("37535960"))
+API_HASH = os.getenv("e89c6a21da912026e645f4132bd4eba7")
+BOT_TOKEN = os.getenv("8721566005:AAHvdIHLgElJRBgdr8WaVaG8UXnndUevZAE")
